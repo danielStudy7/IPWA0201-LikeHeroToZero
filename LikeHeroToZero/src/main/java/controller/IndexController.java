80704f6e717212459b2624c111f6a5de22e5842a
Daniel <daniel-hirt@live.de>
2024-10-19 16:53:12 +0200

    Sortierung final implementieren
    code aufräumen xhtmls

--- LikeHeroToZero/src/main/java/controller/IndexController.java
+++ LikeHeroToZero/src/main/java/controller/IndexController.java
@@ -1,16 +1,10 @@
 package controller;
 
 import java.io.Serializable;
-import java.util.List;
 
-import org.jboss.weld.security.NewInstanceAction;
-
-import dao.EmissionEntryDAO;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lazyDataModel.LazyEmissionEntryDataModel;
-import model.EmissionEntry;
 
 @Named
 @ApplicationScoped
@@ -19,9 +13,6 @@ public class IndexController implements Serializable
 	private static final long serialVersionUID = 1L;
 	
 	private LazyEmissionEntryDataModel lazyDataModel;
-	
-	//@Inject
-	//private EmissionEntryDAO emissionDao;
 	
 	
 	public IndexController()
